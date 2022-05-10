9b93300a9416a6eb86a1d1c89eb28ba6c0978ad4
Zza <1519162203@qq.com>
2022-05-10 17:11:45 +0800

    match update phase 1 finished

--- Codes/Work1/work1.asm
+++ Codes/Work1/work1.asm
@@ -9,38 +9,18 @@ start:
 	mov ss, ax
 	mov sp, 128
 
-	mov ax, offset input_msg_1
+	lea ax, input_msg_1
 	call printf
 
 	call inputf
 
-	mov ax, offset input_msg_2
+	lea ax, input_msg_2
 	call printf
 
 	call outputf
 
 	mov ax, 4c00h
 	int 21H
-
-data segment
-input_msg_1 db "Input the String:", '$'
-input_msg_2 db "Now we get the str:", '$'
-
-; we assume use this to store the str body
-
-; first store the length of buffer
-buffer db 20
-; here we store the len of the gotten str
-db ?
-db "123456789012345678901234567890", '$'
-
-CRLF DB 0AH, 0DH,'$' 
-
-data ends
-
-stack segment
-dw 256 dup(0)
-stack ends
 
 ; proc will use the ax to get the msg
 
@@ -59,16 +39,18 @@ printf endp
 
 inputf proc far
 
-	LEA DX, BUFFER                        ;接收字符串
+	LEA DX, BUFFER
 	MOV AH, 0AH
 	INT 21H
-	MOV AL, BUFFER+1                     ;对字符串进行处理
+	mov al, buffer + 1
+	; MOV AL, ds:[BUFFER+1]
+	; the two above are eq
 	ADD AL, 2
 	MOV AH, 0
 	MOV SI, AX
     MOV BUFFER[SI], '$'
 
-    ; 換行
+; 	CRLF
 	LEA DX,offset CRLF             
 	MOV AH, 09H					 
 	INT 21H
@@ -77,8 +59,8 @@ inputf proc far
 inputf endp
 
 outputf proc far
-
-	LEA DX, BUFFER+2                     ;输出输入的字符串
+; display the inputed str
+	LEA DX, BUFFER+2
 	MOV AH, 09H							 
 	INT 21H
 
@@ -90,5 +72,25 @@ outputf proc far
 outputf endp
 
 code ends
+
+data segment
+input_msg_1 db "Input the String:", '$'
+input_msg_2 db "Now we get the str:", '$'
+
+; we assume use this to store the str body
+
+; first store the length of buffer
+buffer db 20
+; here we store the len of the gotten str
+db ?
+db 20 DUP(0), '$'
+
+CRLF DB 0AH, 0DH,'$' 
+
+data ends
+
+stack segment
+dw 256 dup(0)
+stack ends
 
 end start
